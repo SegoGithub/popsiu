103b9e1f24709b2682a090b2652239ce2c59e29e
Sego <sego@sego.app>
2021-11-23 04:05:30 +0200

    make website faster and improve seo

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import s from '../public/s.png'
 import si from '../public/si.png'
 import Script from 'next/script'
+import Html from 'next/document'
 
 export default function Home() {
   return (
@@ -13,25 +14,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@900&display=swap" rel="stylesheet" />
         <script data-goatcounter="https://sego-siu.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
       </Head>
-      <noscript>
-        <img src="https://sego-siu.goatcounter.com/count?p=/" />
-      </noscript>
-      <main className="home-header">
-        <div id="btn">
-          <Image id="s" src={s} layout="fill" objectFit="cover" quality={100} />
-          <Image id="si" src={si} layout="fill" objectFit="cover" quality={100} />
-        </div>
-        <div className="center">
-          <div className="z-10 center">
-            <h1 className="font-nunito font-black text-8xl text-white thickborder">POPSIU</h1>
-            <h1 id="count" className="font-nunito font-black text-white count">0</h1>
+      <Html lang="en">
+        <noscript>
+          <img src="https://sego-siu.goatcounter.com/count?p=/" />
+        </noscript>
+        <main className="home-header">
+          <div id="btn" className="select-none">
+            <Image id="s" src={s} alt="not siu" layout="fill" objectFit="cover" quality={100} />
+            <Image id="si" src={si} alt="SIIIIIIIIIIIIUUU" layout="fill" objectFit="cover" quality={100} />
           </div>
-        </div>
-      </main>
-      <Script src="/main.js"></Script>
+          <div className="center">
+            <div className="z-10 center">
+              <h1 className="font-nunito font-black text-8xl text-white thickborder">POPSIU</h1>
+              <h1 id="count" className="font-nunito font-black text-white count">0</h1>
+            </div>
+          </div>
+        </main>
+        <Script src="/main.js"></Script>
+      </Html>
     </>
   )
 }
